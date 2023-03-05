76735978a56649debe9d26b8c12b3d1b6420848f
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-03-05 19:08:07 +0100

    flex ok

--- js/busqueda.js
+++ js/busqueda.js
@@ -8,16 +8,22 @@ async function getImages() {
       const imagesContainer = document.getElementById('images-container');
       images.forEach(image => {
           const card = `
-              <div class="col-lg-3 col-md-4 col-sm-6 ">
-                  <div class="card mb-4" style="width: 15rem; height: 45rem;">
-                      <img class="card-img-top" src="http://sulbaranjc.com:3311/${image.namefile}" alt="${image.titulo}" style="width: 15rem; height: 26rem;">
+              <div class="d-flex justify-content-center align-items-center mx-2 pt-5">
+                  <div class="card mb-4" style="width: 21rem; height: 45rem;">
+                      <img class="card-img-top mx-auto pt-2 " src="http://sulbaranjc.com:3311/${image.namefile}" alt="${image.titulo}" style="width: 19.8rem; height: 22rem; border-radius: 15px;
+                      ">
                       <div class="card-body">
                           <h5 class="card-title">${image.titulo}</h5>
                           <p class="card-text">${image.descripcion}</p>
                           <p class="card-text">Precio: $${image.precio}</p>
                           <p class="card-text">Existencia: ${image.existencia}</p>
                       </div>
+                      <div class="card-body d-flex align-items-end">
+                      <button type="button" class="btn btn btn-warning d-block mx-auto mb-3">Añadir a la cesta</button>
+                  </div> 
+
                   </div>
+
               </div>
           `;
           imagesContainer.innerHTML += card;
